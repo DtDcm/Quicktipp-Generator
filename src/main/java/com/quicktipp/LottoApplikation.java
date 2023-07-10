18a11afa8bcf94f25d9878ecc92f0650bc830504
David Tran <david.tran@studium.uni-hamburg.de>
2023-07-10 19:10:29 +0200

    Die Funktion der Eingabe von entweder Lotto oder Eurojackpot wurde hinzugefügt

--- src/main/java/com/quicktipp/LottoApplikation.java
+++ src/main/java/com/quicktipp/LottoApplikation.java
@@ -1,17 +1,40 @@
 package com.quicktipp;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class LottoApplikation 
 {
     public static void main(String[] args)
     {
         List<Integer> list = new ArrayList<>();
-        ZahlenLotterie euro = new Eurojackpot();
-        ZahlenLotterie lotto = new Lotto();
-        euro.generiereTippreihe(list);
-        lotto.generiereTippreihe(list);
+        ZahlenLotterie lotterie;
+
+        //TODO Terminal and User input!
+        System.out.println(">> Geben Sie an, welche Tippreihe Sie verwenden wollen Lotto oder Eurojackpot:");
+        Scanner scanner = new Scanner(System.in);
+        String eingabe = scanner.nextLine();
+
+        try {
+            if(eingabe.equalsIgnoreCase("Lotto") || eingabe.isEmpty()){
+                eingabe = "Lotto";
+                lotterie = new Lotto();
+            } else if (eingabe.equalsIgnoreCase("Eurojackpot")){
+                eingabe = "Eurojackpot";
+                lotterie = new Eurojackpot();
+            }
+            else{
+                throw new InputMismatchException(">> Inkorrekte Eingabe. Bitte geben Sie entweder Lotto oder Eurojackpot an.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println(">> Sie haben " + eingabe + " ausgewählt.");
+
+        scanner.close();
+        
     }
 }
