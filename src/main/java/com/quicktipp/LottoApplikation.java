46717ada82eb9c0f986c6c0c0b3dedebb04e1e28
David Tran <david.tran@studium.uni-hamburg.de>
2023-07-11 13:25:32 +0200

    Funktion der Eingabe von bis zu 6 Unglückszahlen hinzugefügt

--- src/main/java/com/quicktipp/LottoApplikation.java
+++ src/main/java/com/quicktipp/LottoApplikation.java
@@ -7,34 +7,87 @@ import java.util.Scanner;
 
 public class LottoApplikation 
 {
+    private static ZahlenLotterie lotterie;
+    private static List<Integer> unglückszahlen = new ArrayList<>();
     public static void main(String[] args)
     {
-        List<Integer> list = new ArrayList<>();
-        ZahlenLotterie lotterie;
-
-        //TODO Terminal and User input!
         System.out.println(">> Geben Sie an, welche Tippreihe Sie verwenden wollen Lotto oder Eurojackpot:");
         Scanner scanner = new Scanner(System.in);
-        String eingabe = scanner.nextLine();
 
-        try {
-            if(eingabe.equalsIgnoreCase("Lotto") || eingabe.isEmpty()){
-                eingabe = "Lotto";
-                lotterie = new Lotto();
-            } else if (eingabe.equalsIgnoreCase("Eurojackpot")){
-                eingabe = "Eurojackpot";
-                lotterie = new Eurojackpot();
+        überprüfeLotterieEingabe(scanner);
+        
+        System.out.println(">> Geben Sie bis zu 6 Unglückszahlen die ausgeschlossen werden sollen:");
+
+        überprüfeZahlenEingabe(scanner);
+        lotterie.generiereTippreihe(unglückszahlen);
+
+        scanner.close();
+    }
+
+    private static void überprüfeLotterieEingabe(Scanner scanner){
+        boolean istGültig = false;
+        String eingabe = "";
+        while(!istGültig){
+             eingabe = scanner.nextLine();
+            try {
+                if(eingabe.equalsIgnoreCase("Lotto") || eingabe.isEmpty()){
+                    istGültig = true;
+                    eingabe = "Lotto";
+                    lotterie = new Lotto();
+                } else if (eingabe.equalsIgnoreCase("Eurojackpot")){
+                    istGültig = true;
+                    eingabe = "Eurojackpot";
+                    lotterie = new Eurojackpot();
+                }
+                else{
+                    throw new InputMismatchException(">> Inkorrekte Eingabe. Bitte geben Sie entweder Lotto oder Eurojackpot an.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-            else{
-                throw new InputMismatchException(">> Inkorrekte Eingabe. Bitte geben Sie entweder Lotto oder Eurojackpot an.");
+        }
+        System.out.println(">> Sie haben " + eingabe + " ausgewählt.");
+    }
+
+    private static void überprüfeZahlenEingabe(Scanner scanner){
+        boolean istGültig = false;
+        String eingabe = "";
+        while(!istGültig){
+            eingabe = scanner.nextLine();
+            try {
+                String[] zahlEingaben = eingabe.split(" ");
+                if(eingabe.isEmpty()){
+                    break;
+                }
+
+                if(zahlEingaben.length > 6){
+                    throw new InputMismatchException(">> Inkorrekte Eingabe. Sie können nur bis zu 6 Zahlen eingeben.");
+                }
+
+                for (String s : zahlEingaben) {
+                    int zahl = Integer.parseInt(s);
+                    if(lotterie instanceof Lotto){
+                        istGültig = (zahl > 0 || zahl <= 49);
+                    } else if (lotterie instanceof Eurojackpot) {
+                        istGültig = (zahl > 0 || zahl <= 50);
+                    }
+                    
+                    if(!unglückszahlen.contains(zahl) && istGültig){
+                        unglückszahlen.add(zahl);
+                    }
+                    else if(!istGültig){
+                        throw new InputMismatchException(">> Inkorrekte Eingabe. Die Zahl liegt nicht im korrekten Zahlenraum.");
+                    }
+                }
+            } catch (NumberFormatException e){
+                System.out.println(eingabe + " ist keine Zahl. Bitte geben ganze Zahlen an.");
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+                überprüfeZahlenEingabe(scanner);
+            }
         }
         
         System.out.println(">> Sie haben " + eingabe + " ausgewählt.");
-
-        scanner.close();
-        
     }
 }
